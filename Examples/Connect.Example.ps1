7ad47365a062e7d7b361f29e3e15e417992147de
fvanroie <fvanroie@users.noreply.github.com>
2018-02-21 18:56:35 +0100

    Update Examples

--- Examples/Connect.Example.ps1
+++ Examples/Connect.Example.ps1
@@ -6,24 +6,24 @@ $beVerbose = $false
 Import-Module -Name PS_OPNsense -Verbose:$beVerbose
 
 # Define connection parameters
-$key = '<my OPNsense api key>'
-$SecurePassword = '<my OPNsense api secret>' | ConvertTo-SecureString -AsPlainText -Force
-$url = 'https://opnsense.localdomain:80'
+$apiKey = '<my OPNsense api key>'
+$apiPwd = '<my OPNsense api secret>' | ConvertTo-SecureString -AsPlainText -Force
+$Url = 'https://opnsense.localdomain:80'
 
 # Convert the connection parameters into a PowerShell Credentials Object
-$apiCred = New-Object System.Management.Automation.PSCredential -ArgumentList $Key, $SecurePassword
+$apiCred = New-Object System.Management.Automation.PSCredential -ArgumentList $apiKey, $apiPwd
 
 # Specify webcredentials only if you need to use legacy web commands
 # $webCred = Get-Credential -Username 'admin' -Message 'Enter the password for the WebUI:'
 
 # Connect to OPNsense server
 # Note: Use -SkipCertificateCheck if you are using a self-signed certificate
-$conn = Connect-OPNsense -Url $url -Credential $apiCred -SkipCertificateCheck  -Verbose:$beVerbose # -WebCredential $webCred
+$conn = Connect-OPNsense -Url $Url -Credential $apiCred -SkipCertificateCheck -Verbose:$beVerbose  #-WebCredential $webCred
 
 # Display the version
 $conn | Format-Table
 
-# Run the security audit and display the result
+# Run security audit and display the result
 Invoke-OPNsenseAudit -Raw -Verbose:$beVerbose | Format-List
 
 
