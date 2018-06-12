f85d8af6288cd3f4c83a0e45dd17e17cc64b09c5
fvanroie <fvanroie@users.noreply.github.com>
2018-06-12 21:43:09 +0200

    Add skeleton class

---
+++ Classes/Relayd.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Management.Automation;
+
+namespace OPNsense.Rlayd {
+	public class Host {
+		#region Parameters
+		#endregion Parameters
+
+		public Host () {
+		}
+
+		public Host (
+
+		) {
+		}
+	}
+}
+namespace OPNsense.Rlayd {
+	public class Protocol {
+		#region Parameters
+		#endregion Parameters
+
+		public Protocol () {
+		}
+
+		public Protocol (
+
+		) {
+		}
+	}
+}
+namespace OPNsense.Rlayd {
+	public class Table {
+		#region Parameters
+		#endregion Parameters
+
+		public Table () {
+		}
+
+		public Table (
+
+		) {
+		}
+	}
+}
+namespace OPNsense.Rlayd {
+	public class TableCheck {
+		#region Parameters
+		#endregion Parameters
+
+		public TableCheck () {
+		}
+
+		public TableCheck (
+
+		) {
+		}
+	}
+}
+namespace OPNsense.Rlayd {
+	public class VirtualServer {
+		#region Parameters
+		#endregion Parameters
+
+		public VirtualServer () {
+		}
+
+		public VirtualServer (
+
+		) {
+		}
+	}
+}
