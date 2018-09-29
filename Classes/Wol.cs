fae1ad6ef3ee85b73ccead27811666ac4ab6839c
fvanroie <fvanroie@users.noreply.github.com>
2018-09-29 22:25:20 +0200

    Update classes for 18.7

---
+++ Classes/Wol.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Management.Automation;
+
+namespace wol {
+	public class Wolentry {
+		#region Parameters
+		public string descr { get; set; }
+		public PSObject Interface { get; set; }
+		public string mac { get; set; }
+		#endregion Parameters
+
+		public Wolentry () {
+			descr = null;
+			Interface = null;
+			mac = "00:00:00:00:00:00";
+		}
+
+		public Wolentry (
+			string Descr,
+			PSObject Interface_,
+			string Mac
+		) {
+			descr = Descr;
+			Interface = Interface_;
+			mac = Mac;
+		}
+	}
+}
