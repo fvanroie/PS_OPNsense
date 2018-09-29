0cfcdd6b3c0a680e0a8e85d615dd3e55f416e945
fvanroie <fvanroie@users.noreply.github.com>
2018-09-29 22:47:29 +0200

    Update classes for 18.7

---
+++ Classes/Bind.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Management.Automation;
+
+namespace OPNsense.bind.acl.acls {
+	public class Acl {
+		#region Parameters
+		public bool enabled { get; set; }
+		public string name { get; set; }
+		public PSObject networks { get; set; }
+		#endregion Parameters
+
+		public Acl () {
+			enabled = true;
+			name = null;
+			networks = null;
+		}
+
+		public Acl (
+			byte Enabled,
+			string Name,
+			PSObject Networks
+		) {
+			enabled = (Enabled == 0) ? false : true;
+			name = Name;
+			networks = Networks;
+		}
+	}
+}
