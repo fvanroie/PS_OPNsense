5e09299c05a00f21ffdfbd5f3dbe71b376b3c2c2
fvanroie <fvanroie@users.noreply.github.com>
2018-10-04 22:00:44 +0200

    Additional endpoints

--- OPNsensePSDrive.ps1
+++ OPNsensePSDrive.ps1
@@ -42,7 +42,7 @@ class OPNsenseDrive : SHiPSDirectory {
     }
 }
 
-[SHiPSProvider(UseCache = $true)]
+[SHiPSProvider(UseCache = $false)]
 class OPNsenseMenuItem : SHiPSDirectory {
     [string] $item
     [string] $parent
@@ -61,13 +61,88 @@ class OPNsenseMenuItem : SHiPSDirectory {
         $fullPath = $this.parent + $this.name + '/'
 
         Switch ($fullPath) {
+            '/Firewall/Shaper/Pipes/' {
+                $obj += Get-OPNsenseItem -TrafficShaper Pipe
+            }
+            '/Firewall/Shaper/Queues/' {
+                $obj += Get-OPNsenseItem -TrafficShaper Queue
+            }
+            '/Firewall/Shaper/Rules/' {
+                $obj += Get-OPNsenseItem -TrafficShaper Rule
+            }
+
+            '/Lobby/HelloWorld/' {
+                $obj += Get-OPNsenseSetting -Module HelloWorld -Setting General
+            }
+
             '/Services/Freeradius/Clients/' {
                 $obj += Get-OPNsenseItem -Freeradius Client
             }
             '/Services/Freeradius/Users/' {
                 $obj += Get-OPNsenseItem -Freeradius User
             }
-        
+
+            '/Services/Lldpd/' {
+                $newParent = '{0}{1}/' -f $this.parent, $this.name
+                $obj += [OPNsenseMenuItem]::new('General', 'General', $newParent, $null)
+                $obj += [OPNsenseMenuItem]::new('Neighbors', 'Neighbors', $newParent, $null)
+            }
+
+            '/Services/CaptivePortal/Administration/' {
+                $newParent = '{0}{1}/' -f $this.parent, $this.name
+                $obj += [OPNsenseMenuItem]::new('Zones', 'Zones', $newParent, $null)
+                $obj += [OPNsenseMenuItem]::new('Templates', 'Templates', $newParent, $null)
+            }
+            '/Services/CaptivePortal/Administration/Zones/' {
+                $obj += Get-OPNsenseItem -Module CaptivePortal -Item Zone
+            }
+            '/Services/CaptivePortal/Administration/Templates/' {
+                $obj += Get-OPNsenseItem -Module CaptivePortal -Item Template
+            }
+
+            '/Services/Bind/Configuration/' {
+                $newParent = '{0}{1}/' -f $this.parent, $this.name
+                $obj += [OPNsenseMenuItem]::new('General', 'General', $newParent, $null)
+                $obj += [OPNsenseMenuItem]::new('DNSBL', 'DNSBL', $newParent, $null)
+                $obj += [OPNsenseMenuItem]::new('ACLs', 'ACLs', $newParent, $null)
+            }
+            '/Services/Bind/Configuration/General/' {
+                $obj += Get-OPNsenseSetting -Module Bind General
+            }
+            '/Services/Bind/Configuration/DNSBL/' {
+                $obj += Get-OPNsenseSetting -Module Bind DNSBL
+            }
+            '/Services/Bind/Configuration/ACLs/' {
+                $obj += Get-OPNsenseItem -Module Bind -Item Acl
+            }
+
+            '/System/Firmware/Updates/' {
+                $obj += Get-OPNsenseUpdate
+            }
+            '/System/Firmware/Packages/' {
+                $obj += Get-OPNsensePackage -Package
+            }
+            '/System/Firmware/Plugins/' {
+                $obj += Get-OPNsensePackage -Plugin
+            }
+
+            '/Routing/General/' {
+                $obj += Get-OPNsenseSetting -Module Quagga -Setting General
+            }
+            '/Routing/RIP/' {
+                $obj += Get-OPNsenseSetting -Module Quagga -Setting RIP
+            }
+            '/Routing/OSPF/General/' {
+                $obj += Get-OPNsenseSetting -Module Quagga -Setting Ospf
+            }
+
+            '/System/Routes/Configuration/' {
+                $obj += Get-OPNsenseItem -Routes Route
+            }
+            '/System/Routes/Status/' {
+                $obj += Get-OPNsenseRoute
+            }
+
             default {
                 foreach ($item in $this.children) {         
                     #$obj += $pkg
